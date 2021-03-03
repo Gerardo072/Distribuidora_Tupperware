63699fd5dc695f7bf7bbd5d27997c8e6340d52a9
Gerardo072 <78932900+Gerardo072@users.noreply.github.com>
2021-03-03 13:20:56 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,58 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+        final String nombreEmpresa = "Distribuidora Tupperware";
+	
+	    double costoUnitario = 10; // Q45.00
+	    int cantidadProducto = 100;
+	    double totalAPagar = costoUnitario*cantidadProducto; // Q
+	    final double precioDeVenta = 20;
+        final double IVA = 0.12; // 12%
+	    double TotalIVAPorUnidad = precioDeVenta*IVA;
+	    
+	    System.out.println("Costo por unidad: Q"+costoUnitario);
+	    System.out.println("Cantidad de producto: "+cantidadProducto);
+	    System.out.println("-------------------------");
+	    System.out.println("Total: Q"+totalAPagar);
+	    
+	    System.out.println("Venderse a: Q"+precioDeVenta);
+	    System.out.println("Impuesto establecido: "+IVA);
+	    System.out.println("Impuesto por unidad: Q"+TotalIVAPorUnidad);
+	
+	    
+	    int cantidadProductovendido = 1000;
+	    double totalcobradoSinDescuento = cantidadProductovendido * precioDeVenta;
+	    
+	    double totalConDescuento = 0;
+	    
+	    if(cantidadProductovendido>=50) {
+	    	totalConDescuento = totalcobradoSinDescuento - (totalcobradoSinDescuento*0.05);
+	    }
+	    
+	    if(cantidadProductovendido>=100) {
+	        totalConDescuento = totalcobradoSinDescuento - (totalcobradoSinDescuento*0.15);
+	    }
+	    
+	    if(cantidadProductovendido>=1000) {
+	        totalConDescuento = totalcobradoSinDescuento - (totalcobradoSinDescuento*0.25);
+	    }
+	    
+	    
+	    
+	    double impuestototalDeclarar = totalConDescuento *IVA;
+	    double tipoDeDescuento = 0.05;
+	    double ganacias = (totalcobradoSinDescuento - (cantidadProductovendido*costoUnitario))-IVA;
+	    
+	    System.out.println("");
+	    System.out.println("---------------------------------");
+	    System.out.println("Cantidad Vendida: "+cantidadProductovendido);
+	    System.out.println("Total sin descuento: Q"+totalcobradoSinDescuento);
+	    System.out.println("Total con descuento: Q"+totalConDescuento);
+	    System.out.println("IVA a declarar: Q"+impuestototalDeclarar);
+	    System.out.println("Tipo de descuento "+ tipoDeDescuento);
+	    System.out.println("Ganancia: Q"+ ganacias);
+	}
+
+}
